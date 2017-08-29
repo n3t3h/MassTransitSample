a97d767dabf966bb9fa0c41833d428ba03afe1f1
sachindeshpande <sachin.gbox@gmail.com>
2017-08-29 07:54:10 -0700

    Added new tutorial projects

---
+++ MassTransitExample/CloudAMQP/python-amqp-example-master/app.py
@@ -0,0 +1,28 @@
+import pika, os, logging
+logging.basicConfig()
+
+# Parse CLODUAMQP_URL (fallback to localhost)
+#url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://fwlygwfq:Zo6tUo-rOAVEvRRFuWnmq1ZOGcx2cn6v@donkey.rmq.cloudamqp.com/fwlygwfq')
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='hello') # Declare a queue
+# send a message
+channel.basic_publish(exchange='', routing_key='hello', body='Hello CloudAMQP!')
+print(" [x] Sent 'Hello CloudAMQP!'")
+
+# create a function which is called on incoming messages
+def callback(ch, method, properties, body):
+  print(" [x] Received %r" % (body))
+
+# set up subscription on the queue
+channel.basic_consume(callback,
+    queue='hello',
+    no_ack=True)
+
+channel.start_consuming() # start consuming (blocks)
+
+connection.close()
+
