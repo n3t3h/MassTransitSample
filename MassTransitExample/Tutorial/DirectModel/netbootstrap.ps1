a6f91e7769500cc6c404e8db22084d75c24ec332
Sachin Deshpande <sachin.gbox@gmail.com>
2017-08-29 12:48:57 -0700

    Modified Routing example

---
+++ MassTransitExample/Tutorial/DirectModel/netbootstrap.ps1
@@ -0,0 +1,14 @@
+﻿
+dotnet new console --name EmitLogDirect
+cd EmitLogDirect
+ren Program.cs EmitLogDirect.cs
+dotnet add package RabbitMQ.Client
+dotnet restore
+cd ..
+
+dotnet new console --name ReceiveLogsDirect
+cd ReceiveLogsDirect
+ren Program.cs ReceiveLogsDirect.cs
+dotnet add package RabbitMQ.Client
+dotnet restore
+cd..
